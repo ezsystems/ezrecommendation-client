587f94af2ec8379b71e87a402d937493f674782e
Adam Wójs <adam.wojs@ez.no>
2020-05-04 14:20:39 +0200

    EZEE-3137: Changed default value for document_root option

--- src/bundle/DependencyInjection/Configuration.php
+++ src/bundle/DependencyInjection/Configuration.php
@@ -73,7 +73,7 @@ class Configuration extends SiteAccessConfiguration
                         ->end()
                     ->end()
                     ->scalarNode('document_root')
-                        ->defaultValue('%kernel.project_dir%/web/var/export/')
+                        ->defaultValue('%kernel.project_dir%/public/var/export/')
                     ->end()
                 ->end()
             ->end()
