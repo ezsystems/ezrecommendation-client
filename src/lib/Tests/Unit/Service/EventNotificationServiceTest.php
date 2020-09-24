e1304176935fff60c7276bbd3cf585ae19c7d0cc
Tomasz Kryszan <tomasz.kryszan@ez.no>
2020-09-24 14:10:12 +0200

    Added credentials to request body

--- src/lib/Tests/Unit/Service/EventNotificationServiceTest.php
+++ src/lib/Tests/Unit/Service/EventNotificationServiceTest.php
@@ -10,11 +10,13 @@ namespace EzSystems\EzRecommendationClient\Tests\Unit\Service;
 
 use eZ\Publish\API\Repository\Values\Content\ContentInfo;
 use EzSystems\EzRecommendationClient\API\Notifier;
+use EzSystems\EzRecommendationClient\Config\ExportCredentialsResolver;
 use EzSystems\EzRecommendationClient\Config\EzRecommendationClientCredentialsResolver;
 use EzSystems\EzRecommendationClient\Helper\ContentHelper;
 use EzSystems\EzRecommendationClient\Helper\ContentTypeHelper;
 use EzSystems\EzRecommendationClient\Service\EventNotificationService;
 use EzSystems\EzRecommendationClient\Tests\Common\Service\NotificationServiceTest;
+use EzSystems\EzRecommendationClient\Value\Config\ExportCredentials;
 use EzSystems\EzRecommendationClient\Value\Config\EzRecommendationClientCredentials;
 use EzSystems\EzRecommendationClient\Value\EventNotification;
 
@@ -25,6 +27,9 @@ class EventNotificationServiceTest extends NotificationServiceTest
 
     /** @var \EzSystems\EzRecommendationClient\Config\EzRecommendationClientCredentialsResolver|\PHPUnit\Framework\MockObject\MockObject */
     private $credentialsResolverMock;
+
+    /** @var \EzSystems\EzRecommendationClient\Config\ExportCredentialsResolver|\PHPUnit\Framework\MockObject\MockObject */
+    private $exportCredentialsMock;
 
     /** @var \EzSystems\EzRecommendationClient\Helper\ContentHelper|\PHPUnit\Framework\MockObject\MockObject */
     private $contentHelperMock;
@@ -37,12 +42,14 @@ class EventNotificationServiceTest extends NotificationServiceTest
         parent::setUp();
 
         $this->credentialsResolverMock = $this->createMock(EzRecommendationClientCredentialsResolver::class);
+        $this->exportCredentialsMock = $this->createMock(ExportCredentialsResolver::class);
         $this->contentHelperMock = $this->createMock(ContentHelper::class);
         $this->contentTypeHelperMock = $this->createMock(ContentTypeHelper::class);
         $this->notificationService = new EventNotificationService(
             $this->clientMock,
             $this->loggerMock,
             $this->credentialsResolverMock,
+            $this->exportCredentialsMock,
             $this->contentHelperMock,
             $this->contentTypeHelperMock
         );
@@ -70,6 +77,14 @@ class EventNotificationServiceTest extends NotificationServiceTest
                 'customerId' => 12345,
                 'licenseKey' => '12345-12345-12345-12345',
             ]));
+
+        $this->exportCredentialsMock
+            ->method('getCredentials')
+            ->willReturn(new ExportCredentials([
+                'login' => 12345,
+                'password' => '12345-12345-12345-12345',
+            ]));
+
         $this->clientMock
             ->method('__call')
             ->with(Notifier::API_NAME, [])
