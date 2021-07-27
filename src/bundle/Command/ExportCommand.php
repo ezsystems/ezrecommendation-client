5424bdb3d28a01bc2ac935147c1c963d66cbff82
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-07-27 10:50:09 +0200

    IBX-201: Refactored ExportCommand

--- src/bundle/Command/ExportCommand.php
+++ src/bundle/Command/ExportCommand.php
@@ -6,12 +6,13 @@
  */
 declare(strict_types=1);
 
-namespace EzSystems\EzRecommendationClientBundle\Command;
+namespace Ibexa\Bundle\Personalization\Command;
 
 use eZ\Bundle\EzPublishCoreBundle\Command\BackwardCompatibleCommand;
-use EzSystems\EzRecommendationClient\Factory\ExportParametersFactoryInterface;
 use EzSystems\EzRecommendationClient\Http\HttpEnvironmentInterface;
 use EzSystems\EzRecommendationClient\Service\ExportServiceInterface;
+use Ibexa\Personalization\Export\Input\CommandInputResolverInterface;
+use Ibexa\Personalization\Factory\Export\ParametersFactoryInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -25,53 +26,45 @@ final class ExportCommand extends Command implements BackwardCompatibleCommand
 {
     public const SUCCESS = 0;
 
-    /** @var \EzSystems\EzRecommendationClient\Service\ExportServiceInterface */
-    private $exportService;
+    private ExportServiceInterface $exportService;
 
-    /** @var \EzSystems\EzRecommendationClient\Http\HttpEnvironmentInterface */
-    private $httpEnvironment;
+    private HttpEnvironmentInterface $httpEnvironment;
 
-    /** @var \Psr\Log\LoggerInterface */
-    private $logger;
+    private LoggerInterface $logger;
 
-    /** @var \EzSystems\EzRecommendationClient\Factory\ExportParametersFactoryInterface */
-    private $exportParametersFactory;
+    private CommandInputResolverInterface $inputResolver;
+
+    private ParametersFactoryInterface $exportParametersFactory;
 
     public function __construct(
         ExportServiceInterface $exportService,
         HttpEnvironmentInterface $httpEnvironment,
         LoggerInterface $logger,
-        ExportParametersFactoryInterface $exportParametersFactory
+        CommandInputResolverInterface $inputResolver,
+        ParametersFactoryInterface $exportParametersFactory
     ) {
         parent::__construct();
 
         $this->exportService = $exportService;
         $this->httpEnvironment = $httpEnvironment;
         $this->logger = $logger;
+        $this->inputResolver = $inputResolver;
         $this->exportParametersFactory = $exportParametersFactory;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function configure()
+    protected function configure(): void
     {
         $this
             ->setName('ibexa:recommendation:run-export')
             ->setAliases(['ezrecommendation:export:run'])
             ->setDescription('Run export to files.')
-            ->addOption('webHook', null, InputOption::VALUE_OPTIONAL, 'Guzzle Client base_uri parameter, will be used to send recommendation data')
+            ->addOption('customer-id', null, InputOption::VALUE_REQUIRED, 'Personalization customer id')
+            ->addOption('license-key', null, InputOption::VALUE_REQUIRED, 'Personalization license key')
+            ->addOption('web-hook', null, InputOption::VALUE_OPTIONAL, 'Recommendation engine URI used to send recommendation data')
             ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'Host used in exportDownload url for notifier in export feature')
-            ->addOption('customerId', null, InputOption::VALUE_OPTIONAL, 'Your eZ Recommendation customer ID')
-            ->addOption('licenseKey', null, InputOption::VALUE_OPTIONAL, 'Your eZ Recommendation license key')
-            ->addOption('pageSize', null, InputOption::VALUE_OPTIONAL, '', 500)
-            ->addOption('page', null, InputOption::VALUE_OPTIONAL, '', 1)
-            ->addOption('path', null, InputOption::VALUE_OPTIONAL, 'A string of subtree path, eg: /1/2/')
-            ->addOption('hidden', null, InputOption::VALUE_OPTIONAL, 'If set to 1 - Criterion Visibility: VISIBLE will be used', 0)
-            ->addOption('image', null, InputOption::VALUE_OPTIONAL, 'Image_variations used for images')
-            ->addOption('contentTypeIdList', null, InputOption::VALUE_REQUIRED, 'List of Content Types ID')
-            ->addOption('fields', null, InputOption::VALUE_OPTIONAL, 'List of the fields, eg: title, description')
-        ;
+            ->addOption('item-type-identifier-list', null, InputOption::VALUE_REQUIRED, 'List of item types identifiers')
+            ->addOption('languages', null, InputOption::VALUE_REQUIRED, 'List of items languages')
+            ->addOption('page-size', null, InputOption::VALUE_OPTIONAL, '', '500');
     }
 
     /**
@@ -84,14 +77,13 @@ final class ExportCommand extends Command implements BackwardCompatibleCommand
 
             date_default_timezone_set('UTC');
 
-            $options = array_diff_key(
-                $input->getOptions(),
-                $this->getApplication()->getDefinition()->getOptions()
-            );
-            $options['siteaccess'] = $input->getOption('siteaccess');
-
-            $this->exportService->process(
-                $this->exportParametersFactory->create($options),
+            $this->exportService->runExport(
+                $this->exportParametersFactory->create(
+                    $this->inputResolver->resolve(
+                        $input, $this->getApplication()
+                    ),
+                    ParametersFactoryInterface::COMMAND_TYPE
+                ),
                 $output
             );
 
---
+++ tests/bundle/Command/ExportCommandTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Bundle\Personalization\Command;
+
+use EzSystems\EzRecommendationClient\Exception\InvalidArgumentException;
+use EzSystems\EzRecommendationClient\Exception\MissingExportParameterException;
+
+final class ExportCommandTest extends AbstractCommandTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected static function getCommandName(): string
+    {
+        return 'ibexa:recommendation:run-export';
+    }
+
+    public function testThrowExceptionWhenInvalidSiteAccessIsPassed(): void
+    {
+        $siteAccess = 'undefined_siteaccess';
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            sprintf('SiteAccess %s doesn\'t exists', $siteAccess)
+        );
+        $this->commandTester->execute(
+            [
+                '--siteaccess' => $siteAccess,
+            ]
+        );
+    }
+
+    /**
+     * @param array<array<array<string>|string>> $parameters
+     *
+     * @dataProvider provideForTestCommandThrowExceptionWhenRequiredParametersAreMissing
+     */
+    public function testThrowExceptionWhenRequiredParametersAreMissing(
+        array $parameters,
+        string $expectExceptionMessage
+    ): void {
+        $this->expectException(MissingExportParameterException::class);
+        $this->expectExceptionMessage($expectExceptionMessage);
+        $this->commandTester->execute($parameters);
+    }
+
+    /**
+     * @return array<array<array<string>|string>>.
+     */
+    public function provideForTestCommandThrowExceptionWhenRequiredParametersAreMissing(): array
+    {
+        return [
+            [
+                [
+                    '--item-type-identifier-list' => 'product, article, blog_post',
+                    '--languages=eng-GB',
+                    '--customer-id' => '12345',
+                ],
+                'Required parameters: license-key, siteaccess are missing',
+            ],
+            [
+                [
+                    '--item-type-identifier-list' => 'product, article, blog_post',
+                    '--languages=eng-GB',
+                    '--license-key' => '12345-12345-12345-12345',
+                ],
+                'Required parameters: customer-id, siteaccess are missing',
+            ],
+            [
+                [
+                    '--item-type-identifier-list' => 'product, article, blog_post',
+                    '--languages=eng-GB',
+                    '--siteaccess' => 'first_siteaccess',
+                ],
+                'Required parameters: customer-id, license-key are missing',
+            ],
+        ];
+    }
+}
