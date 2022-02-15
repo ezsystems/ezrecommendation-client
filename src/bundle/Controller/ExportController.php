eca6c3d29cbdad037cfcf02170d54ed4e6ae9ca6
Tomasz Kryszan <tomasz.kryszan@ez.no>
2022-02-15 11:34:21 +0100

    Dropped run export action

--- src/bundle/Controller/ExportController.php
+++ src/bundle/Controller/ExportController.php
@@ -10,13 +10,7 @@ namespace EzSystems\EzRecommendationClientBundle\Controller;
 
 use EzSystems\EzPlatformRest\Server\Controller;
 use EzSystems\EzRecommendationClient\Authentication\AuthenticatorInterface;
-use EzSystems\EzRecommendationClient\Exception\ExportInProgressException;
 use EzSystems\EzRecommendationClient\File\FileManagerInterface;
-use EzSystems\EzRecommendationClient\Helper\ExportProcessRunnerHelper;
-use EzSystems\EzRecommendationClient\Value\ExportRequest;
-use Psr\Log\LoggerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 final class ExportController extends Controller
@@ -27,22 +21,12 @@ final class ExportController extends Controller
     /** @var \EzSystems\EzRecommendationClient\File\FileManagerInterface */
     private $fileManager;
 
-    /** @var \EzSystems\EzRecommendationClient\Helper\ExportProcessRunnerHelper */
-    private $exportProcessRunner;
-
-    /** @var \Psr\Log\LoggerInterface */
-    private $logger;
-
     public function __construct(
         AuthenticatorInterface $authenticator,
-        FileManagerInterface $fileManager,
-        ExportProcessRunnerHelper $exportProcessRunner,
-        LoggerInterface $logger
+        FileManagerInterface $fileManager
     ) {
         $this->authenticator = $authenticator;
         $this->fileManager = $fileManager;
-        $this->exportProcessRunner = $exportProcessRunner;
-        $this->logger = $logger;
     }
 
     /**
@@ -65,31 +49,5 @@ final class ExportController extends Controller
         $response->setContent($content);
 
         return $response;
-    }
-
-    /**
-     * @ParamConverter("export_request_converter")
-     *
-     * @throws \EzSystems\EzRecommendationClient\Exception\ExportInProgressException
-     */
-    public function exportAction(ExportRequest $request): JsonResponse
-    {
-        $response = new JsonResponse();
-
-        if (!$this->authenticator->authenticate()) {
-            return $response->setStatusCode(Response::HTTP_UNAUTHORIZED);
-        }
-
-        if ($this->fileManager->isLocked()) {
-            $this->logger->warning('Export is running.');
-            throw new ExportInProgressException('Export is running');
-        }
-
-        $this->exportProcessRunner->run($request->getExportRequestParameters());
-
-        return $response->setData([sprintf(
-            'Export started at %s',
-            date('Y-m-d H:i:s')
-        )]);
     }
 }
