287c8654e8049ddcbc3b18281e1f280e31c13b88
Tomasz Kryszan <tomasz.kryszan@ez.no>
2022-02-16 11:58:57 +0100

    IBX-2330: Added usage remoteId in user tracker
    https://issues.ibexa.co/browse/IBX-2330

--- src/bundle/Templating/Twig/Functions/UserTracking.php
+++ src/bundle/Templating/Twig/Functions/UserTracking.php
@@ -8,83 +8,107 @@ declare(strict_types=1);
 
 namespace EzSystems\EzRecommendationClientBundle\Templating\Twig\Functions;
 
+use eZ\Publish\API\Repository\Values\Content\Content;
 use eZ\Publish\Core\MVC\ConfigResolverInterface;
 use eZ\Publish\Core\MVC\Symfony\Locale\LocaleConverterInterface;
 use EzSystems\EzRecommendationClient\Helper\ContentTypeHelper;
 use EzSystems\EzRecommendationClient\Service\UserServiceInterface;
 use EzSystems\EzRecommendationClient\Value\Parameters;
+use Ibexa\Personalization\Config\Repository\RepositoryConfigResolverInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Twig\Environment as TwigEnvironment;
 use Twig\Extension\RuntimeExtensionInterface;
 
-final class UserTracking extends AbstractFunction implements RuntimeExtensionInterface
+final class UserTracking implements RuntimeExtensionInterface
 {
     /** @var \eZ\Publish\Core\MVC\ConfigResolverInterface */
     private $configResolver;
 
+    /** @var \EzSystems\EzRecommendationClient\Helper\ContentTypeHelper */
+    private $contentTypeHelper;
+
     /** @var \eZ\Publish\Core\MVC\Symfony\Locale\LocaleConverterInterface */
     private $localeConverter;
 
-    /** @var \EzSystems\EzRecommendationClient\Service\UserServiceInterface */
-    private $userService;
-
-    /** @var \EzSystems\EzRecommendationClient\Helper\ContentTypeHelper */
-    private $contentTypeHelper;
+    /** @var \Ibexa\Personalization\Config\Repository\RepositoryConfigResolverInterface */
+    private $repositoryConfigResolver;
 
     /** @var \Symfony\Component\HttpFoundation\RequestStack */
     private $requestStack;
 
+    /** @var \Twig\Environment */
+    private $twig;
+
+    /** @var \EzSystems\EzRecommendationClient\Service\UserServiceInterface */
+    private $userService;
+
     public function __construct(
         ConfigResolverInterface $configResolver,
-        LocaleConverterInterface $localeConverter,
-        UserServiceInterface $userService,
         ContentTypeHelper $contentTypeHelper,
+        LocaleConverterInterface $localeConverter,
+        RepositoryConfigResolverInterface $repositoryConfigResolver,
         RequestStack $requestStack,
-        TwigEnvironment $twig
+        TwigEnvironment $twig,
+        UserServiceInterface $userService
     ) {
-        parent::__construct($twig);
-
         $this->configResolver = $configResolver;
         $this->contentTypeHelper = $contentTypeHelper;
         $this->localeConverter = $localeConverter;
+        $this->repositoryConfigResolver = $repositoryConfigResolver;
         $this->requestStack = $requestStack;
+        $this->twig = $twig;
         $this->userService = $userService;
     }
 
     /**
-     * Renders simple tracking snippet code.
+     * @throws \Exception
+     */
+    public function trackUser(Content $content): ?string
+    {
+        $contentInfo = $content->contentInfo;
+        if ($this->contentTypeHelper->isContentTypeExcluded($contentInfo)) {
+            return null;
+        }
+
+        $contentId = $this->repositoryConfigResolver->useRemoteId() ? $contentInfo->remoteId : $content->id;
+
+        return $this->render(
+            [
+                'contentId' => $contentId,
+                'contentTypeId' => $content->getContentType()->id,
+            ]
+        );
+    }
+
+    /**
+     * @phpstan-param array{
+     *  'contentId': string|int,
+     *  'contentTypeId': int,
+     * } $context
      *
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
-     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
-     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
      */
-    public function trackUser(int $contentId): string
+    private function render(array $context): string
     {
-        $includedContentTypes = $this->configResolver->getParameter('included_content_types', Parameters::NAMESPACE);
         $customerId = $this->configResolver->getParameter('authentication.customer_id', Parameters::NAMESPACE);
-
-        if (!\in_array($this->contentTypeHelper->getContentTypeIdentifier($contentId), $includedContentTypes)) {
-            return '';
-        }
 
         return $this->twig->render(
             '@EzRecommendationClient/track_user.html.twig',
-            [
-                'contentId' => $contentId,
-                'contentTypeId' => $this->contentTypeHelper->getContentTypeId(
-                    $this->contentTypeHelper->getContentTypeIdentifier($contentId)
-                ),
-                'language' => $this->localeConverter->convertToEz($this->requestStack->getCurrentRequest()->get('_locale')),
-                'userId' => $this->userService->getUserIdentifier(),
-                'customerId' => $customerId,
-                'consumeTimeout' => $this->getConsumeTimeout(),
-                'trackingScriptUrl' => $this->configResolver->getParameter(
-                    Parameters::API_SCOPE . '.event_tracking.script_url',
-                    Parameters::NAMESPACE
-                ),
-            ]
+            array_merge(
+                [
+                    'language' => $this->localeConverter->convertToEz($this->requestStack->getCurrentRequest()->get('_locale')),
+                    'userId' => $this->userService->getUserIdentifier(),
+                    'customerId' => $customerId,
+                    'consumeTimeout' => $this->getConsumeTimeout(),
+                    'trackingScriptUrl' => $this->configResolver->getParameter(
+                        Parameters::API_SCOPE . '.event_tracking.script_url',
+                        Parameters::NAMESPACE
+                    ),
+                ],
+                $context
+            )
         );
     }
 
